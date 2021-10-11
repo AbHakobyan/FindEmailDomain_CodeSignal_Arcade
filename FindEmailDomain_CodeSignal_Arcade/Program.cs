05342a705d3604c3e567b124dcdac10f0d0e035c
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-11 12:50:41 +0400

    Добавьте файлы проекта.

---
+++ FindEmailDomain_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEmailDomain_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "sajdkjaks@google.com";
+            string v = findEmailDomain(s);
+            Console.WriteLine(v);
+        }
+
+        static string findEmailDomain(string address)
+        {
+            string[] s = address.Split('@');
+            return s[s.Length - 1];
+        }
+    }
+}
